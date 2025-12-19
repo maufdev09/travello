3bf8c6c9d72eb5902300797ce993cce5ef5bdc93
Maruf Ahmed <marufdev09@gmail.com>
2025-12-19 23:12:34 +0600

    feat: Add authentication utility functions and route configurations

---
+++ src/lib/authUtils.ts
@@ -0,0 +1,77 @@
+export type userRole = "TOURIST" | "GUIDE" | "ADMIN";
+
+export type RouteConfig = {
+  exact: string[];
+  patterns: RegExp[];
+};
+
+export const authRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/reset-password",
+];
+
+export const commonProtectedRoutes: RouteConfig = {
+  exact: ["/my-profile", "/settings"],
+  patterns: [],
+};
+
+const guideProtectedRoutes: RouteConfig = {
+  patterns: [/^\/guide/],
+  exact: [],
+};
+
+export const adminProtectedRoutes: RouteConfig = {
+  patterns: [/^\/admin/],
+  exact: [],
+};
+
+const touristProtectedRoutes: RouteConfig = {
+  patterns: [/^\/dashboard/],
+  exact: [],
+};
+
+export const isAuthRoute = (pathname: string) => {
+  return authRoutes.some((route) => route === pathname);
+};
+export const isRouteMatched = (
+  pathname: string,
+  routes: RouteConfig
+): boolean => {
+  if (routes.exact.includes(pathname)) {
+    return true;
+  }
+
+  return routes.patterns.some((pattern) => pattern.test(pathname));
+};
+
+export const getRouteOwner = (pathname: string): userRole | "COMMON" | null => {
+  if (isRouteMatched(pathname, adminProtectedRoutes)) {
+    return "ADMIN";
+  }
+  if (isRouteMatched(pathname, guideProtectedRoutes)) {
+    return "GUIDE";
+  }
+  if (isRouteMatched(pathname, touristProtectedRoutes)) {
+    return "TOURIST";
+  }
+  if (isRouteMatched(pathname, commonProtectedRoutes)) {
+    return "COMMON";
+  }
+  return null;
+};
+
+export const getDefaultDashboardRoute = (role: userRole): string => {
+  if (role === "ADMIN") {
+    return "/admin/dashboard";
+  }
+  if (role === "GUIDE") {
+    return "/guide/dashboard";
+  }
+
+  if (role === "TOURIST") {
+    return "/dashboard";
+  }
+  return "/";
+};
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,83 +1,13 @@
-import { i } from "framer-motion/client";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { cookies } from "next/headers";
-
-type userRole = "TOURIST" | "GUIDE" | "ADMIN";
-
-type RouteConfig = {
-  exact: string[];
-  patterns: RegExp[];
-};
-
-const authRoutes = [
-  "/login",
-  "/register",
-  "/forgot-password",
-  "/reset-password",
-];
-
-const commonProtectedRoutes: RouteConfig = {
-  exact: ["/my-profile", "/settings"],
-  patterns: [],
-};
-
-const guideProtectedRoutes: RouteConfig = {
-  patterns: [/^\/guide/],
-  exact: [],
-};
-
-const adminProtectedRoutes: RouteConfig = {
-  patterns: [/^\/admin/],
-  exact: [],
-};
-
-const touristProtectedRoutes: RouteConfig = {
-  patterns: [/^\/dashboard/],
-  exact: [],
-};
-
-const isAuthRoute = (pathname: string) => {
-  return authRoutes.some((route) => route === pathname);
-};
-const isRouteMatched = (pathname: string, routes: RouteConfig): boolean => {
-  if (routes.exact.includes(pathname)) {
-    return true;
-  }
-
-  return routes.patterns.some((pattern) => pattern.test(pathname));
-};
-
-const getRouteOwner = (pathname: string): userRole | "COMMON" | null => {
-  if (isRouteMatched(pathname, adminProtectedRoutes)) {
-    return "ADMIN";
-  }
-  if (isRouteMatched(pathname, guideProtectedRoutes)) {
-    return "GUIDE";
-  }
-  if (isRouteMatched(pathname, touristProtectedRoutes)) {
-    return "TOURIST";
-  }
-  if (isRouteMatched(pathname, commonProtectedRoutes)) {
-    return "COMMON";
-  }
-  return null;
-};
-
-const getDefaultDashboardRoute = (role: userRole): string => {
-  if (role === "ADMIN") {
-    return "/admin/dashboard";
-  }
-  if (role === "GUIDE") {
-    return "/guide/dashboard";
-  }
-
-  if (role === "TOURIST") {
-    return "/dashboard";
-  }
-  return "/";
-};
+import {
+  getDefaultDashboardRoute,
+  getRouteOwner,
+  isAuthRoute,
+  userRole,
+} from "./lib/authUtils";
 
 export async function proxy(request: NextRequest) {
   const cookieStore = await cookies();
@@ -101,49 +31,47 @@ export async function proxy(request: NextRequest) {
     }
 
     userRole = verifiedToken.role;
-
-
   }
-  const  routeOwner= getRouteOwner(pathname);
+  const routeOwner = getRouteOwner(pathname);
 
-  const isAuth=isAuthRoute(pathname);
+  const isAuth = isAuthRoute(pathname);
 
   if (accessToken && isAuth) {
     return NextResponse.redirect(
       new URL(getDefaultDashboardRoute(userRole as userRole), request.url)
     );
-    
   }
 
-  if (routeOwner === null ) {
+  if (routeOwner === null) {
     return NextResponse.next();
   }
 
   if (!accessToken) {
-
-    const loginUrl = new URL('/login', request.url);
-    loginUrl.searchParams.set('redirect', pathname);
-      return NextResponse.redirect(loginUrl)
-    }
-
+    const loginUrl = new URL("/login", request.url);
+    loginUrl.searchParams.set("redirect", pathname);
+    return NextResponse.redirect(loginUrl);
+  }
 
   if (routeOwner === "COMMON") {
     if (!accessToken) {
-      return NextResponse.redirect(new URL('/login', request.url))
+      return NextResponse.redirect(new URL("/login", request.url));
     }
-    return NextResponse.next()
+    return NextResponse.next();
   }
 
-
-if (routeOwner==="ADMIN"|| routeOwner==="GUIDE"|| routeOwner==="TOURIST") {
+  if (
+    routeOwner === "ADMIN" ||
+    routeOwner === "GUIDE" ||
+    routeOwner === "TOURIST"
+  ) {
     if (userRole !== routeOwner) {
       return NextResponse.redirect(
         new URL(getDefaultDashboardRoute(userRole as userRole), request.url)
       );
     }
 
-  return NextResponse.next();
-}
+    return NextResponse.next();
+  }
   return NextResponse.next();
 }
 export const config = {
